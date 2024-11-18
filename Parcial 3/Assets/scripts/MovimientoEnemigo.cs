c1adbfedda6fdacb0cb6170122500838c42018ac
davidalvarezrestrepo <david.alvarez_res@uao.edu.co>
2024-11-17 20:38:54 -0500

    sonido zombies

--- Parcial 3/Assets/scripts/MovimientoEnemigo.cs
+++ Parcial 3/Assets/scripts/MovimientoEnemigo.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class MovimientoEnemigo : MonoBehaviour
 {
     public Transform player;
-    private NavMeshAgent agent; 
+    private NavMeshAgent agent;
     public Animator anim;
 
     public float rango;
-    public bool atacando;
-    private bool persiguiendo = false;
+
+    public AudioSource sonido;
     
+
+    //private bool persiguiendo = false;
+
     //para atacar(quitar vida)
     public float salud = 150f;
     public int damageGolpe;
+
+    float Distancia;
 
     void Awake()
     {
@@ -25,95 +31,95 @@ public class MovimientoEnemigo : MonoBehaviour
 
     void Start()
     {
+        sonido.Play();
         agent.enabled = false; // Desactivamos el NavMeshAgent al inicio
     }
 
     void Update()
     {
+        Distancia = Vector3.Distance(transform.position, player.position);
+
         if (gameObject.CompareTag("Player"))
         {
-            if(salud < 0){
+            if (salud < 0)
+            {
                 salud = 0;
             }
         }
-        if (persiguiendo)
+
+        if (Distancia < 8)
         {
+            //persiguiendo = true;
+            agent.enabled = true; // Activamos el NavMeshAgent para que persiga
+
             PerseguirJugador();
         }
+
+       
     }
 
     private void PerseguirJugador()
     {
-        if(Vector3.Distance(transform.position, player.transform.position) > rango){
-            
+        
+        if (Distancia > rango)
+        {
             agent.destination = player.position;
             anim.SetBool("Walk", true);
-            anim.SetBool("atacar", false); 
-            atacando = false;
-        }else{
-            Debug.Log("ENTRO A UNA DISTANCIA MENOR");
-            agent.isStopped = true; // Pausar el movimiento
-            anim.SetBool("atacar", true);
-            //atacando = true;
-            agent.destination = transform.position;
-            StartCoroutine(waitAnim());
-            
-            
-            
-            
-        
-            
-
+            anim.SetBool("atacar", false);
         }
+        else
+        {
+            anim.SetBool("atacar", true);
+            agent.destination = transform.position;
+            agent.isStopped = true; // Pausar el movimiento
+            StartCoroutine(waitAnim());
+        }
+
     }
 
     private IEnumerator waitAnim()
     {
-        
-        yield return new WaitForSeconds(2.12f);
-        atacando = true;
-        agent.isStopped = false; // Reanudar el movimiento
-        //agent.speed = 0;
-        
-        
-        
-    }
 
+        yield return new WaitForSeconds(2.12f);
+        agent.isStopped = false; // Reanudar el movimiento
+
+    }
 
     //BAJAR VIDA
     public void getDamageZ(int dmg)
     {
         salud -= dmg;
         Debug.Log("Rebajó" + dmg + "de vida." + dmg + "/" + salud);
-    
+
         if (salud <= 0)
         {
             Muerte();
         }
     }
 
-    public void Muerte(){
-        Debug.Log("Zombie ha Muerto");
+    public void Muerte()
+    {
+        Debug.Log("Ha Muerto zombie");
         anim.SetBool("Muerte", true);
 
         agent.isStopped = true;
+        sonido.Pause();
+
     }
-
-
-    // Método para detectar cuando el jugador entra al rango del enemigo
+    // Método para detectar cuando el jugador entra al rangos del enemigo
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+
+        /*if (other.CompareTag("Player"))
+        if (Distancia < 30)
         {
             persiguiendo = true;
             agent.enabled = true; // Activamos el NavMeshAgent para que persiga
-        }
+        }*/
 
-        if (other.CompareTag("Player") && atacando)
+        if (other.CompareTag("Player") /*&& atacando*/)
         {
             other.GetComponent<MovPersonaje>().getDamageP(damageGolpe);
         }
-
-    }
-    
+    } 
 }
